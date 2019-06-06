646093facde093c001453adc3ac39de8722afad2
MIchael Berry <michaeladamberry3@gmail.com>
2019-06-06 13:08:05 -0400

    checking in first design

---
+++ data.js
@@ -0,0 +1,70 @@
+//primary source in making difficulty assessment:
+//https://www.backpacker.com/trips/ask-a-thru-hiker-triple-crown-of-hiking
+
+let data = [
+  {
+    name: "The Appalachian Trail",
+    //in miles
+    distance: 2200,
+    //lat long coords
+    start: [34.6272, -84.1939],
+    end: [45.9044, -68.9216],
+    //max elevation in feet
+    maxElevation: 6643,
+    minElevation: 124,
+    difficulty: 0.7
+  },
+  {
+    name: "The Continental Divide Trail",
+    distance: 3100,
+    start: [31.635278, -108.398889],
+    end: [49.050278, -113.881389],
+    //max elevation in feet
+    maxElevation: 14278,
+    minElevation: 3900,
+    difficulty: 0.9
+  },
+  {
+    name: "The Pacific Crest Trail",
+    distance: 2653,
+    //lat long coords
+    start: [32.659722, -116.502222],
+    end: [49.066667, -120.783333],
+    //max elevation in feet
+    maxElevation: 13153,
+    minElevation: 140,
+    difficulty: 0.6
+  }
+
+  //TODO gather data of remainder of NSTs
+  //   arizona: {
+  //     distance: 800
+  //   },
+
+  //   florida: {
+  //     distance: 1300
+  //   },
+  //   iceAge: {
+  //     distance: 1200
+  //   },
+  //   natchezTrace: {
+  //     distance: 450
+  //   },
+  //   newEngland: {
+  //     distance: 215
+  //   },
+  //   northCountry: {
+  //     distance: 4600,
+  //     start: [43.9503352, -73.4370667],
+  //     end: [47.523889, -101.451944],
+  //     maxElevation: 1,
+  //     minElevation: 1
+  //   },
+
+  //   pacificNorthwest: {
+  //     distance: 1200
+  //   },
+  //   potomacHeritage: {
+  //     distance: 710
+  //   }
+];
